45058baa12fe7884189561cd18cfb5d31ac995bb
Redacted <107178843+redactederror@users.noreply.github.com>
2026-06-21 20:14:11 +0100

    Add ROM size selection to configuration view

--- src/views/Configuration.vue
+++ src/views/Configuration.vue
@@ -7,35 +7,57 @@
         @click="handleChangeConfiguration"
         label="Show score at bottom of screen"
       />
+
       <v-switch
         v-model="configurationState.showBlankLines"
         @click="handleChangeConfiguration"
         label="Show blank lines between background rows"
       />
+
+      <v-select
+        v-model="configurationState.romSize"
+        :items="romSizeOptions"
+        label="ROM Size"
+        @change="handleChangeConfiguration"
+      />
     </v-card-text>
-    </v-card>
+  </v-card>
 </template>
+
 <script>
 import {computed, defineComponent} from '@vue/composition-api';
 
 import {useConfigurationStorage} from '../hooks/project';
 
 export default defineComponent({
-  setup(props, context) {
+  setup() {
     const configurationStorage = useConfigurationStorage();
+
+    const romSizeOptions = [
+      '4k',
+      '8k',
+      '16k',
+      '32k',
+      '64k',
+    ];
 
     const configurationState = computed({
       get() {
         const DEFAULT_CONFIGURATION = {
           showScore: true,
           showBlankLines: true,
+          romSize: '8k',
         };
 
         try {
-          const configuration = configurationStorage.value || structuredClone(DEFAULT_CONFIGURATION);
+          const configuration =
+            configurationStorage.value ||
+            structuredClone(DEFAULT_CONFIGURATION);
 
-          return Object.fromEntries(Object.entries(DEFAULT_CONFIGURATION)
-              .map(([k, v]) => [k, configuration[k] ?? v]));
+          return Object.fromEntries(
+            Object.entries(DEFAULT_CONFIGURATION)
+                .map(([k, v]) => [k, configuration[k] ?? v]),
+          );
         } catch (e) {
           console.error('Error loading configuration from local storage', e);
           return structuredClone(DEFAULT_CONFIGURATION);
@@ -51,9 +73,11 @@ export default defineComponent({
       configurationState.value = configurationState.value;
     };
 
-    return {configurationState, handleChangeConfiguration};
-  },
-  methods: {
+    return {
+      configurationState,
+      handleChangeConfiguration,
+      romSizeOptions,
+    };
   },
 });
 </script>
